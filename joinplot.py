74e76c53dd545cbf57de78823e47998ccad33878
chris wiggins <chrishwiggins@users.noreply.github.com>
2023-08-11 21:13:15 -0400

    adding y=x

--- joinplot.py
+++ joinplot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Yelp and Google ratings
 yelp_ratings = [4.53, 3.7, 4.2, 4.0, 4.5, 3.9]
@@ -14,8 +15,16 @@ plt.scatter(google_ratings, yelp_ratings, label="Shops")
 for i, name in enumerate(names):
     plt.text(google_ratings[i], yelp_ratings[i], name, fontsize=8)
 
-plt.xlabel("Google Ratings")
-plt.ylabel("Yelp Ratings")
-plt.title("Scatter Plot of Yelp vs Google Ratings")
+# Red line y=x
+min_val = min(min(google_ratings), min(yelp_ratings))
+max_val = max(max(google_ratings), max(yelp_ratings))
+x = np.linspace(min_val, max_val, 100)
+plt.plot(x, x, color='red')
+
+plt.xlim(min_val, max_val)
+plt.ylim(min_val, max_val)
+plt.xlabel('Google Ratings')
+plt.ylabel('Yelp Ratings')
+plt.title('Scatter Plot of Yelp vs Google Ratings')
 plt.legend()
 plt.show()
