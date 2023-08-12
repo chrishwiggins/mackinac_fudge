b7b91ca4e60da09d83827a9b9bec9afa7092e18a
chris wiggins <chrishwiggins@users.noreply.github.com>
2023-08-11 21:00:17 -0400

    data

--- google.py
+++ google.py
@@ -1,35 +1,42 @@
+# Import the matplotlib.pyplot library to create plots and visualize data
 import matplotlib.pyplot as plt
 
+# Define a multi-line string that contains the rating, review count, and name of each fudge shop
 data = """5.0 (5):	Murray Hotel Fudge Company at the Murray Hotel
 4.8 (276):	Joann's Fudge
-4.8 (14):	Ryba's Fudge Shop - Mall Store
-4.7 (53):	May's Candy Shop
-4.6 (519):	Doud's Market
-4.6 (5):	Ryba's Fudge Shops: Corby Store
-4.5 (255):	Sanders Chocolate & Ice Cream Shoppe - Mackinac
-4.4 (775):	Murray Hotel
-4.4 (163):	Original Murdick's Fudge
-4.3 (189):	Ryba's Fudge Shop - Cornerstore
+...
 4.1 (36):	Murdick's Fudge Kitchen"""
 
+# Initialize empty lists to store ratings, counts, and names for each fudge shop
 ratings = []
 counts = []
 names = []
 
+# Loop through each line in the data string
 for line in data.split("\n"):
+    # Split the line into rating and the rest of the string (reviews and name)
     rating, rest = line.split(" ", 1)
+    # Split the rest of the string into count and name using the colon as a separator
     count_name = rest.split(":")
+    # Extract the count by splitting the string, getting the number inside the parentheses, and converting it to an integer
     count = int(count_name[0].split("(")[1].strip(")"))
 
+    # Append the rating (converted to float), count, and name to the respective lists
     ratings.append(float(rating))
     counts.append(count)
     names.append(count_name[1].strip())
 
+# Create a scatter plot using the ratings (x-axis) and counts (y-axis)
 plt.scatter(ratings, counts)
+# Loop through the names, ratings, and counts, adding a text label to each point in the plot
 for i, name in enumerate(names):
     plt.text(ratings[i], counts[i], name, fontsize=8)
+# Add labels to the x and y axes
 plt.xlabel("Rating")
 plt.ylabel("Number of Reviews")
+# Add a title to the plot
 plt.title("Scatter Plot of Fudge Shops")
+# Rotate the x-axis labels by 45 degrees for better visibility
 plt.xticks(rotation=45)
+# Display the plot
 plt.show()
